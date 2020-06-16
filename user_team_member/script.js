3889907b59a83d4ed6f8b101c8e44e0c9deeb0d2
prinxard <prinxard@gmail.com>
2020-06-16 11:01:56 +0100

    update

--- user_team_member/script.js
+++ user_team_member/script.js
@@ -6,17 +6,13 @@ $(document).ready(function () {
           success: (function (members) {
               $.each(members.data, function (i, data) {
                   
-                  $('#tdata').append("<tr>" +
-                      `<td> <img src= ${data.avatar} class='rounded-circle'>  </img> </td>`+
-                      `<td> ${data.first_name} </td>`+
-                      `<td> ${data.email} </td>`+
-                      `<td> Developer  </td>`+
-                      `<td> <button class='btn btn-primary'>  Activated  </button>  </td>` +
-                      `<td> <img src=${'./images/icons8-settings-16.png'}>   </img> </td>` +
-                      `<td> <img src=${'./images/icons8-sort-down-16.png'}>   </img> </td>` 
-                       
-                      
-                      
+                  $('#tmembers').append("<tr class='tmember'>" + 
+                      `<td class='tmember-avatar'> <img src= ${data.avatar} class='rounded-circle avatar'>  </img> </td>` +
+                      `<td class='tmember-name'> ${data.first_name} </td>`+
+                      `<td class='tmember-email'> ${data.email} </td>`+
+                      `<td class='tmember-position'> Developer  </td>`+
+                      `<td class='tmember-status'> <button class='btn btn-primary'>  Activated  </button>  </td>` +
+                      `<td class='icons'> <img src=${'./images/icons8-settings-16.png'}>  </img> <img src=${'./images/icons8-sort-down-16.png'}>   </img> </td>`         
                   +"</tr>")
               })
           }),
